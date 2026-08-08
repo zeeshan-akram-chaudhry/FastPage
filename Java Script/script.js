6bcf89533ef6d0d781f106936f3c401268503d85
Zeeshan Akram <za3819486@gmail.com>
2026-08-08 19:37:03 +0500

    Login page completed

---
+++ Java Script/script.js
@@ -0,0 +1,14 @@
+function togglePassword() {
+    const password = document.getElementById("password");
+    const icon = document.querySelector(".password_toggle i");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
